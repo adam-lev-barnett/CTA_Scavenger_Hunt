47b1ba0fb5cf31ccc0b627fef505b8704469a42b
adam-lev-barnett <adam.lev.barnett@gmail.com>
2026-03-01 09:41:06 -0600

    Fix database connection to retrieve nearby points of interest for stations

--- src/main/java/com/hackathon/chica_go/controller/StationController.java
+++
@@ -1,9 +0,0 @@
-package com.hackathon.chica_go.controller;
-
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-
-@RestController
-@RequestMapping("/stations")
-public class StationController {
-}
