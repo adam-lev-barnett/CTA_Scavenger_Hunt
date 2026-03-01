efe8e04d40fdd2d212922ba416ab11d1e9f906b6
Yung Han Jeong <yungh.jeong@gmail.com>
2026-03-01 02:25:54 -0600

    commit before modifying db

--- src/SQL/etl/Parser.py
+++ src/SQL/etl/Parser.py
@@ -1,7 +1,11 @@
 import os
 import json
+import numpy as np
 
-def Parser(filename):
+def Parser(filename, point=10):
+    """
+    outputs a list of dict
+    """
     base_dir = os.path.dirname(os.path.abspath(__file__))
     file_path = os.path.join(base_dir, filename)
 
@@ -22,13 +26,53 @@ def Parser(filename):
             lon, lat = coordinates
             results.append({
                 "name": name,
-                "latitude": lat,
-                "longitude": lon
+                "latitude": float(lat),
+                "longitude": float(lon),
+                "points": point
             })
 
     return results
 
+def get_nearest_station(poi:dict, stations:list) -> dict:
+    """
+    """
+    _stations = []
+    poi_pt = np.array((poi['latitude'], poi['longitude']))
+    for station in stations:
+        station_pt = np.array((station['latitude'], station['longitude']))
+        _dist = np.linalg.norm(poi_pt - station_pt)
+        _stations.append((_dist, station['name']))
+    
+    _stations = list(sorted(_stations, key=lambda x: x[0]))
+    return _stations[0][1] # lowest _dst, name
+
+def build_poi_list(poi_filepath, stations_filepath) -> tuple:
+    """
+    return a list of dicts from both datasets
+    """
+    stations = Parser(stations_filepath)
+    _pois = Parser(poi_filepath, 5)
+    pois = []
+    for _poi in _pois:
+        pois.append(
+            {
+                "name": _poi["name"],
+                "latitude": float(_poi["latitude"]),
+                "longitude": float(_poi["longitude"]),
+                "nearest_station": get_nearest_station(_poi, stations),
+                "points": _poi["points"]
+            }
+        )
+
+    return (stations, pois)
+
+
+def test_nearest_station():
+    pois = Parser("poi.geojson")
+    stations = Parser("stations.geojson", 5)
+    return get_nearest_station(pois[0], stations)
 
 if __name__ == "__main__":
-    stations = Parser("stations.geojson")
-    print(stations)
+    _out = build_poi_list("poi.geojson", "stations.geojson")
+    print("stations: ", _out[0])
+    print("pois: ", _out[1])
